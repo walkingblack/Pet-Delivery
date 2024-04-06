16da7418d4022a521252e371e12cf80b9c93c01b
Antonio Jesuz <antonio.jesuz@hotmail.com>
2024-04-06 13:31:05 -0300

    petdelivery

---
+++ src/enviar.js
@@ -0,0 +1,7 @@
+function enviar {
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != ""){
+        alert (`Obrigado Sr(a)${nome.value}os seus dados foram encaminhados com sucesso`);
+    }
+}
